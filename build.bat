55e985b158186dac7121bf2f8c1dd880a2a0b1c2
Simono-cmd <trauth.szymon@gmail.com>
2025-06-05 16:57:25 +0200

    added docker

--- build.bat
+++ build.bat
@@ -1,1 +1,13 @@
+
+@echo off
+echo Uruchamiam docker-compose...
+docker-compose up -d
+if %ERRORLEVEL% neq 0 (
+    echo Blad podczas uruchamiania docker-compose
+    pause
+    exit /b %ERRORLEVEL%
+)
+echo docker-compose uruchomiony pomyslnie.
+pause
+
 mvn clean install
